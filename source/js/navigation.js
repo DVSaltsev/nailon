e866c3cf63ebe6084c6e34155606e80793745f37
DVSaltsev <DVSaltsev@gmail.com>
2022-08-31 11:26:39 +0500

    разметка услуг

--- source/js/navigation.js
+++ source/js/navigation.js
@@ -1,9 +1,14 @@
-let navWrapper = document.querySelector('.header__nav-wrapper');
-let navToggle = document.querySelector('.header__nav-toggle');
+let navMain = document.querySelector('.header-top__nav');
+let navToggle = document.querySelector('.header-top__toggle');
 
 // navMain.classList.remove('main-nav--nojs');
 
 navToggle.addEventListener('click', function () {
-  navWrapper.classList.toggle('header__nav--closed');
-  navWrapper.classList.toggle('header__nav--opened');
+  if (navMain.classList.contains('header-top__nav--closed')) {
+    navMain.classList.remove('header-top__nav--closed');
+    navMain.classList.add('header-top__nav--opened');
+  } else {
+    navMain.classList.add('header-top__nav--closed');
+    navMain.classList.remove('header-top__nav--opened');
+  }
 });
